c42886bc652c457d125d43152811b86c8703821c
andreslargo <felipeflogxd>
2020-06-20 17:09:45 -0500

    Util file con get

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,11 @@
-import { API_KEY, API } from 'react-native-dotenv'
+import { API_KEY, API } from 'react-native-dotenv';
 
 const axios = require('axios');
 
 export const fetchData = {
-  OpenGet: async function(country, method) {
+  OpenGet: async function(country, method, limit = 10, page) {
     return await axios
-      .get(`${API}/2.0/?method=${method}&country=${country}&api_key=${API_KEY}&format=json`)
+      .get(`${API}/2.0/?method=${method}&country=${country}&api_key=${API_KEY}&format=json&limit=${limit}&page=${page}`)
       .then((res) => createOkResponse(res))
       .catch((err) => createErrorResponse(err));
   }
