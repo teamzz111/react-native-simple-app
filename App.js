1089de7be26fe18f6e50c0e46cc3bc98676272bd
andreslargo <felipeflogxd>
2020-06-20 13:08:54 -0500

    Configuración de íconos

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
 import { AppNavigator }  from './src/navigation/root';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -12,6 +13,8 @@ const HomeScreen = () => (
 
 export default App = () => (
   <ApplicationProvider {...eva} theme={eva.light}>
+    <IconRegistry icons={EvaIconsPack} />
+
     <AppNavigator/>
   </ApplicationProvider>
 );
