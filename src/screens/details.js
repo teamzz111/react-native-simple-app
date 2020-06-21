871f6b21ae8828d24365e6d00c9d8a7cb69e193e
andreslargo <felipeflogxd>
2020-06-21 04:10:32 -0500

    Detalles dinñámicos

--- src/screens/details.js
+++ src/screens/details.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import { StyleSheet, Linking } from "react-native";
-import { Text, Icon, Button, Layout, TopNavigation, TopNavigationAction, Avatar } from '@ui-kitten/components';
+import { StyleSheet, View } from "react-native";
+import { Text, Icon, Linking, Button, Layout, TopNavigation, TopNavigationAction, Avatar } from '@ui-kitten/components';
 
 const Details = ({route, navigation}) => {
   const infiniteAnimationIconRef = React.useRef();
   const zoomIconRef = React.useRef();
+  const [data, setData] = useState(route.params?.item);
 
   React.useEffect(() => {
     infiniteAnimationIconRef.current.startAnimation();
@@ -37,13 +38,14 @@ const Details = ({route, navigation}) => {
   );
 
 
-  const [data, setData] = useState(route.params?.item);
   const BackAction = () => (
     <TopNavigationAction icon={BackIcon} onPress = {() => navigation.goBack(null)}/>
   );
 
-  const goToUrl = () => {
-    const url = data.url;
+
+  
+  const goToUrl = (ur) => {
+    const url = !ur ? data.url : ur;
     Linking.canOpenURL(url).then(supported => {
       if (supported) {
         Linking.openURL(url);
@@ -52,6 +54,7 @@ const Details = ({route, navigation}) => {
       }
     });
   };
+
   return(
     <>
         <TopNavigation
@@ -61,7 +64,7 @@ const Details = ({route, navigation}) => {
         <Layout style={styles.container} level='1'>
             <Avatar style={styles.avatar} size='giant' source={{uri: data.image[2]["#text"]}}/>
             <Text style={styles.text} category='h4'>{data.name}</Text>
-            <Text category='s2'>{format(data.listeners)} of listeners</Text>
+            <Text category='s2'>{format(data.listeners)} of listeners - {data.duration ? 'with duration of ' + data.duration : '' }</Text>
             {
               data.streamable == 0 ?
                 <Button
@@ -71,7 +74,7 @@ const Details = ({route, navigation}) => {
                   accessoryRight={renderInfiniteAnimationIcon}>
                   NOT STREAMABLE
                 </Button>
-              :
+              : data.streamable  == 1 ? 
                 <Button
                   appearance='ghost'
                   status='info'
@@ -79,15 +82,70 @@ const Details = ({route, navigation}) => {
                   accessoryRight={renderInfiniteAnimationIcon}>
                   STREAMABLE
                 </Button>
-
+              : data.streamable['#text'] == 0 ? 
+                <Button
+                  appearance='ghost'
+                  status='info'
+                  style={styles.button}
+                  accessoryRight={renderInfiniteAnimationIcon}>
+                  NOT STREAMABLE
+                </Button>
+                :
+                <Button
+                  appearance='ghost'
+                  status='info'
+                  style={styles.button}
+                  accessoryRight={renderInfiniteAnimationIcon}>
+                   STREAMABLE
+                </Button>
             } 
-            <Button
-              style={styles.button}
-              accessoryLeft={renderZoomIcon}
-              onPress={() => {zoomIconRef.current.startAnimation(); goToUrl();}}>
-              WATCH ON LAST.FM
-            </Button>
-            <Text style = {{fontSize: 10}} category='s2'>MBID: {data.mbid}</Text>
+            {
+              data.streamable['fulltrack'] &&
+                  <Text category='s2'>FullTrack: {data.streamable['fulltrack']}      {'\n'}</Text>
+            }
+              <Button
+                style={styles.button}
+                accessoryLeft={renderZoomIcon}
+                onPress={() => {zoomIconRef.current.startAnimation(); goToUrl(null);}}>
+                WATCH ON LAST.FM
+              </Button>
+              <Text style = {{fontSize: 10}} category='s2'>MBID: {data.mbid}</Text>
+
+            {
+              
+              route.params.id == 'tracks' ? 
+              <>
+
+              <View
+                style={{
+                  borderBottomColor: 'black',
+                  borderBottomWidth: .5,
+                  marginTop: 20,
+                  width: '86%'
+                }}
+              />
+              <View style={{flexDirection: 'row'}}>
+                <View style = {styles.center}>
+                  <Icon
+                    style={styles.icon}
+                    fill='#dca65c'
+                    name='award-outline'
+                    />
+                  <Text style={styles.text} category='h5'>Rank #{(parseInt(data['@attr'].rank)+1)}</Text>
+                </View>
+                <View style = {styles.center}>
+                  <Text category= 's1'>Artist: {data.artist.name}</Text>  
+                  <Button size = 'small' style={styles.button} appearance='outline' onPress = {() => goToUrl(data.artist.url)}>
+                    DETAILS
+                  </Button> 
+  
+                </View>
+              </View>
+              <Text style = {{fontSize: 10}} category='s2'>Artist MBID: {data.artist.mbid}</Text>
+              </>
+              : <View/>
+            }
+
         </Layout>
     </>
   );
@@ -95,7 +153,18 @@ const Details = ({route, navigation}) => {
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    padding: 20
+    padding: 20,
+    margin: 20,
+    borderRadius: 10, 
+    elevation: 3
+  },
+  center: {
+    justifyContent: 'center', alignItems: 'center', alignSelf: 'center', padding: 10,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    marginTop: 10
   },
   avatar: {
     margin: 8,
